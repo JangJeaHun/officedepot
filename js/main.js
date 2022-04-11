88c31b951f3f16ee38f1216d21114ff1759fa818
J <wkdwogns2345@naver.com>
2022-04-12 08:40:01 +0900

    0412

--- js/main.js
+++ js/main.js
@@ -9,40 +9,44 @@ $(document).ready(function () {
 
 
 
-    $(".mainSlide").slick({
-    infinite:true,
-    arrows:false,
-    dots:true,
-    autoplay:true,
-    autoplaySpeed:4000
-    });
+    // $(".mainSlide").slick({
+    // infinite:true,
+    // arrows:false,
+    // dots:true,
+    // autoplay:true,
+    // autoplaySpeed:4000
+    // });
+
     // .main_bottom>ul>li>ul>li>a:hover{
     //     border-bottom: solid 3px red;
     // }
 
-    $(".main_bottom>ul>li>ul>li>a").hover(function(){
-        $(this).addClass("on").css({
-            borderBottom: "solid 3px red"
-        });
+    // $(".main_bottom>ul>li>ul>li>a").hover(function(){
+    //     $(this).addClass("on").css({
+    //         borderBottom: "solid 3px red"
+    //     });
 
         
-    },function(){
-        $(this).css({
-            borderBottom: "1px solid #eee"
-        })
-        $(".main_bottom>ul>li a").removeClass();
-    })
+    // },function(){
+    //     $(this).css({
+    //         borderBottom: "1px solid #eee"
+    //     })
+    //     $(".main_bottom>ul>li a").removeClass();
+    // })
 
-    $(".listA>a").mouseover(function () {
-        var li_on = $(this).parent().parent().parent().attr("data-group-filter");
-        var li_on_num = $(this).parent().eq("li").index();
+    // $(".listA>a").mouseover(function () {
+    //     var li_on = $(this).parent().parent().parent().attr("data-group-filter");
+    //     var li_on_num = $(this).parent().eq("li").index();
 
-        console.log(li_on_num);
+    //     console.log(li_on_num);
     //   if(data-group-filter==0 & ){
             
     //   }
 
-    });
+    // });
 
+
+
+    
 
 });
