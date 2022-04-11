f447fd347c21e017b9eaa91dfce486935591e8cb
J <wkdwogns2345@naver.com>
2022-04-11 18:55:02 +0900

    0411

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
-$(document).ready(function(){
-    $("#allMenuBtn").click(function(){
-        $(".allMenu").show();
-    })
-    
-    $(".allMenu_close").click(function(){
-        $(".allMenu").hide();
-        
-    })
-    
-    // $('.main_slide').slick({
-    //     fade:true,
-    //     pauseOnHover:true,
-    //     autoplay:true,
-    //     autoplaySpeed:3000
-    // });
+$(document).ready(function () {
+  $("#allMenuBtn").click(function () {
+    $(".allMenu").show();
+  });
 
-    var idx = $(".main_bottom>ul a").index();
-    alert(idx);
+  $(".allMenu_close").click(function () {
+    $(".allMenu").hide();
+  });
 
-    $(".main_bottom>ul>li>ul>li>a").eq(idx).mouseover(function(){
-        $(".mainSlide>div").eq(idx).fadeIn().siblings("div").fadeOut();
-    })
+  $(".mainSlide").slick({
+    fade: true,
+    pauseOnHover: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
 
-})
+  $(".tab>li>a").mouseover(function () {});
+});
